37c67bae50b505e37c2ea4c640cbbd2dafeb9d1e
91812 <ankit_b180639me@nitc.ac.in>
2021-06-20 21:21:21 +0530

    delete1.sql

---
+++ delete1.sql
@@ -0,0 +1,8 @@
+select * from subjects;
+select * from books_subjects where subject=9;
+delete from books_subjects
+where book=8;
+delete from books where id=8;
+delete from subjects where id=9;
+
+
